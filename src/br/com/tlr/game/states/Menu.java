b127206f01b811b280ef210b8c6ecaa4c9907c8a
Perin <perin@rech.com.br>
2016-10-13 16:52:24 -0300

    Merge origin/master
    Estrutura das classes

--- src/br/com/tlr/game/states/Menu.java
+++ src/br/com/tlr/game/states/Menu.java
@@ -4,12 +4,14 @@
  */
 package br.com.tlr.game.states;
 
+import java.awt.Button;
 import org.newdawn.slick.AngelCodeFont;
 import org.newdawn.slick.Color;
 import org.newdawn.slick.Font;
 import org.newdawn.slick.GameContainer;
 import org.newdawn.slick.Graphics;
 import org.newdawn.slick.Image;
+import org.newdawn.slick.Input;
 import org.newdawn.slick.SlickException;
 import org.newdawn.slick.state.BasicGameState;
 import org.newdawn.slick.state.StateBasedGame;
@@ -23,6 +25,22 @@ public class Menu extends BasicGameState {
     private Image background;
     /** Fonte padrão para escrever textos */
     private Font texto;
+
+    private Image btImg;
+
+    /** Botão play */
+    private Button btPlay;
+    // Utilizar Nifty GUI
+    // https://sourceforge.net/projects/nifty-gui/files/latest/download
+    // http://pontov.com.br/site/java/48-java2d/327-criando-uma-interface-ao-usuario-usando-nifty-gui
+    // https://docs.jmonkeyengine.org/advanced/nifty_gui.html
+    // http://vimeo.com/25637085
+
+    
+    // Try alternatives like Nifty GUI which is compatible with slick
+    // Pesquisar por Slick2D button no google
+    // Ler http://stackoverflow.com/questions/17970481/slick2d-gui-button-listener
+    // https://www.youtube.com/watch?v=lLYzToAaGQQ
 
     @Override
     public int getID() {
@@ -40,6 +58,8 @@ public class Menu extends BasicGameState {
     public void init(GameContainer container, StateBasedGame game) throws SlickException {
         background = new Image("data/menu/bg-squares-3d.jpg");
         texto = new AngelCodeFont("org/newdawn/slick/data/defaultfont.fnt", "org/newdawn/slick/data/defaultfont.png");
+        btPlay = new Button("Teste");
+        btImg = new Image("data/menu/bt1.png");
     }
 
     /**
@@ -52,7 +72,17 @@ public class Menu extends BasicGameState {
      */
     @Override
     public void update(GameContainer container, StateBasedGame game, int delta) throws SlickException {
+        Input input = container.getInput();
 
+        int x = 0;
+        int y = 0;
+
+        String msg = "  --- ";
+        if(input.getMouseX() > x && input.getAbsoluteMouseY() > y && input.getMouseX() < x + btImg.getWidth()
+            && input.getMouseY() < y + btImg.getHeight()){
+            msg = "em cima do bt";
+        }
+        System.out.println(msg);
     }
 
     /**
@@ -66,7 +96,9 @@ public class Menu extends BasicGameState {
     @Override
     public void render(GameContainer container, StateBasedGame game, Graphics g) throws SlickException {
         background.draw();
+        btImg.draw();
         texto.drawString(150f, 50f, "TESTE STRING", Color.yellow);
+//        btPlay.p
     }
 
 }
